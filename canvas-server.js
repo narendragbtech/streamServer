4f48bbecb5430eb709602bb6ec4a12b78e374d20
SoftwareClan <53153286+SoftwareClan@users.noreply.github.com>
2021-05-31 10:57:13 +0530

    update camera update

--- canvas-server.js
+++ canvas-server.js
@@ -4,37 +4,60 @@ const app = require('express')()
     , rtsp = require('rtsp-ffmpeg');
 ;
 // use rtsp = require('rtsp-ffmpeg') instead if you have install the package
-server.listen(3000, function () {
-    console.log('Listening on localhost:3000');
+server.listen(80, function () {
+    console.log('Listening on localhost:80');
 });
 var cams = [
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/101'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/201'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/301'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/401'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/501'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/601'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/701'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/801'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/901'
-    , 'rtsp://admin:ADMIN@12@136.233.89.172:554/Streaming/Channels/1001'
+    {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/101', code: '1024 | 101'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/201', code: '1024 | 201'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/301', code: '1024 | 301'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/401', code: '1024 | 401'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/501', code: '1024 | 501'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/601', code: '1024 | 601'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/701', code: '1024 | 701'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/801', code: '1024 | 801'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/901', code: '1024 | 901'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1001', code: '1024 | 1001'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1101', code: '1024 | 1101'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1201', code: '1024 | 1201'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1301', code: '1024 | 1301'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1401', code: '1024 | 1401'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1501', code: '1024 | 1501'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1601', code: '1024 | 1601'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1701', code: '1024 | 1701'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1801', code: '1024 | 1801'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1901', code: '1024 | 1901'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/2001', code: '1024 | 2001'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/2101', code: '1024 | 2101'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/2201', code: '1024 | 2201'}
+
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/101', code: '1025 | 101'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/201', code: '1025 | 201'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/301', code: '1025 | 301'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/401', code: '1025 | 401'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/501', code: '1025 | 501'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/601', code: '1025 | 601'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/701', code: '1025 | 701'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/801', code: '1025 | 801'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/901', code: '1025 | 901'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/1001', code: '1025 | 1001'}
 
 ].map(function (uri, i) {
     var stream = new rtsp.FFMpeg({
-        input: uri,
-        resolution: '320x240',
-        quality: 3,
+        input: uri.url,
+        resolution: '704*576',
+        quality: 12,
         arguments: ['-rtsp_transport', 'tcp']
     });
-    let url=uri.split("/");
-    let channel_number = url[url.length-1];
+
+    let channel_number = uri.code;
     stream.on('start', function () {
         console.log('stream ' + channel_number + ' started');
     });
     stream.on('stop', function () {
         console.log('stream ' + channel_number + ' stopped');
     });
-    return {stream : stream,channel:channel_number};
+    return {stream: stream, channel: channel_number};
 });
 
 cams.forEach(function (object, i) {
@@ -54,7 +77,8 @@ cams.forEach(function (object, i) {
 });
 
 io.on('connection', function (socket) {
-    socket.emit('start', cams.length);
+    let code = cams.map(i=>i.channel);
+    socket.emit('start', code);
 });
 
 app.get('/', function (req, res) {
