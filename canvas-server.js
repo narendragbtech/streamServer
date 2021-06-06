d088fb2848bb993d4ae1b5c5e69a36a369a93033
SoftwareClan <53153286+SoftwareClan@users.noreply.github.com>
2021-06-06 10:45:16 +0530

    old version

--- canvas-server.js
+++ canvas-server.js
@@ -1,101 +1,124 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const rtsp = require('rtsp-ffmpeg');
 const path = require('path');
 const fs = require('fs');
-const {spawn} = require("child_process");
+const server = require('http').Server(app)
+    , io = require('socket.io')(server)
+    , rtsp = require('rtsp-ffmpeg'),
+    exec = require('child_process').exec;
+// use rtsp = require('rtsp-ffmpeg') instead if you have install the package
+server.listen(3000, function () {
+    console.log('Listening on localhost:80');
+});
 
-app.use(express.static(path.join("public")));
+app.use(express.static(path.join(__dirname, "video")));
 
-let filenames = [];
-readCameras().then(canvasList => {
-    let streamList = canvasList.map(function (uri, i) {
-        let stream = new rtsp.FFMpeg({
-            input: uri.url,
-            resolution: '704*576',
-            quality: 5,
-            arguments: ['-rtsp_transport', 'tcp']
-        });
+var cams = [
+    {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/101', code: '1024 | 101'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/201', code: '1024 | 201'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/301', code: '1024 | 301'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/401', code: '1024 | 401'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/501', code: '1024 | 501'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/601', code: '1024 | 601'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/701', code: '1024 | 701'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/801', code: '1024 | 801'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/901', code: '1024 | 901'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1001', code: '1024 | 1001'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1101', code: '1024 | 1101'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1201', code: '1024 | 1201'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1301', code: '1024 | 1301'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1401', code: '1024 | 1401'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1501', code: '1024 | 1501'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1601', code: '1024 | 1601'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1701', code: '1024 | 1701'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1801', code: '1024 | 1801'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/1901', code: '1024 | 1901'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/2001', code: '1024 | 2001'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/2101', code: '1024 | 2101'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/2201', code: '1024 | 2201'}
 
-        let channel_number = uri.image;
-        stream.on('start', function () {
-            console.log('stream ' + channel_number + ' started');
-        });
-        stream.on('stop', function () {
-            console.log('stream ' + channel_number + ' stopped');
-        });
-        return {stream: stream, channel: channel_number};
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/101', code: '1025 | 101'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/201', code: '1025 | 201'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/301', code: '1025 | 301'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/401', code: '1025 | 401'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/501', code: '1025 | 501'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/601', code: '1025 | 601'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/701', code: '1025 | 701'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/801', code: '1025 | 801'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/901', code: '1025 | 901'}
+    , {url: 'rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/1001', code: '1025 | 1001'}
+
+].map(function (uri, i) {
+    var stream = new rtsp.FFMpeg({
+        input: uri.url,
+        resolution: '704*576',
+        quality: 12,
+        arguments: ['-rtsp_transport', 'tcp']
     });
 
-    streamList.forEach(function (object, i) {
-        let ns = io.of('/' + object.channel);
-        ns.on('connection', function (wsocket) {
-            console.log('connected to /cam' + object.channel);
-            var pipeStream = function (data) {
-                wsocket.emit('data', data);
-            };
-            object.stream.on('data', pipeStream);
-            wsocket.on('disconnect', function () {
-                console.log('disconnected from /cam' + object.channel);
-                object.stream.removeListener('data', pipeStream);
-            });
-        });
-
+    let channel_number = uri.code;
+    stream.on('start', function () {
+        console.log('stream ' + channel_number + ' started');
     });
-}).catch(error=>{
-    console.log(error)
+    stream.on('stop', function () {
+        console.log('stream ' + channel_number + ' stopped');
+    });
+    return {stream: stream, channel: channel_number};
+});
+
+cams.forEach(function (object, i) {
+    var ns = io.of('/' + object.channel);
+    ns.on('connection', function (wsocket) {
+        console.log('connected to /cam' + object.channel);
+        var pipeStream = function (data) {
+            wsocket.emit('data', data);
+        };
+        object.stream.on('data', pipeStream);
+
+        wsocket.on('disconnect', function () {
+            console.log('disconnected from /cam' + object.channel);
+            object.stream.removeListener('data', pipeStream);
+        });
+    });
 });
 
 io.on('connection', function (socket) {
-    socket.emit('capture', filenames);
+    let number = Math.random();
+    let code = cams.map(i => i.channel);
+    socket.emit('start', code);
+    //  getImage('rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/401','./public/picture-'+number+'.jpg', (type,object)=>{
+    //      console.log('execution',type);
+    //      if(type ===2){
+    //          let code = cams.map(i => i.channel);
+    //            fs.readFile(__dirname+"/public/picture-"+number+".jpg",(err,data)=>{
+    //                if(!err){
+    //                    console.log(data);
+    //                    socket.emit('start',data.toString('base64'));
+    //                }
+    //                console.log(err);
+    //              return;
+    //          })
+    //      }
+    //  });
+
 });
 
-app.get('/', async function (req, res) {
-    let AllChildProcess = await captureStreamImages();
-    Promise.all(AllChildProcess).then(r => {
-        console.log(r);
-        res.sendFile(__dirname + '/index-canvas.html');
-    }).catch(error => {
-        console.log(error);
-    });
-
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index-canvas.html');
 });
-
-async function captureStreamImages() {
-    let cameras = await readCameras();
-    filenames = [];
-    if (!cameras) {
-        console.log("camera list not available");
-        return null;
-    }
-    return cameras.map(cam => {
-        return new Promise((resolve, reject) => {
-            let filename = `./public/images/picture_${cam.image}.jpg`;
-            let ls = spawn("ffmpeg",
-                ["-rtsp_transport", "tcp", "-i", cam.url, "-vframes","10" , "-q:v", "5","-f","image2" ,"-update", "1", "-y", filename]);
-            ls.on("close", code => {
-                console.log(`child process exited with code ${code}`);
-                filenames.push({image:`images/picture_${cam.image}.jpg`,code:"/"+cam.image,number:cam.image});
-                resolve(1);
-            });
-            ls.on('error', function (err) {
-                if (err.code === 'ENOENT') {
-                    reject(new Error('FFMpeg executable wasn\'t found. Install this package and check FFMpeg.cmd property'));
-                } else {
-                    reject(err);
-                }
-            });
-        });
-    });
+// "ffmpeg -rtsp_transport tcp -i rtsp://admin:ADMIN@12@136.233.89.172:1024/Streaming/Channels/401 -vframes 5 -update 1 ./public/picture-1024-401.jpg"
+function getImage(url,file_name,callback) {
+    let args = ['ffmpeg','-rtsp_transport tcp -i ',url,'-vframes 5','-update 1',file_name];
+    console.log(args.join(" "));
+    execute(args.join(" "),callback);
 }
 
-async function readCameras() {
-    let jsonData = await fs.readFileSync(__dirname + "/camera.json");
-    return JSON.parse(jsonData);
+function execute(command, callback) {
+    exec(command, function (error, stdout, stderr) {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return ;
+        }
+        return callback(2,stdout);
+    });
 }
-
-server.listen(80, function () {
-    console.log('Listening on localhost:80');
-});
--- server2.js
+++ server2.js
@@ -15,7 +15,7 @@ readCameras().then(canvasList => {
         let stream = new rtsp.FFMpeg({
             input: uri.url,
             resolution: '704*576',
-            quality: 12,
+            quality: 5,
             arguments: ['-rtsp_transport', 'tcp']
         });
 
@@ -77,7 +77,7 @@ async function captureStreamImages() {
                 ["-rtsp_transport", "tcp", "-i", cam.url, "-vframes","10" , "-q:v", "5","-f","image2" ,"-update", "1", "-y", filename]);
             ls.on("close", code => {
                 console.log(`child process exited with code ${code}`);
-                filenames.push({image:`images/picture_${cam.image}.jpg`,code:"/"+cam.image});
+                filenames.push({image:`images/picture_${cam.image}.jpg`,code:"/"+cam.image,number:cam.image});
                 resolve(1);
             });
             ls.on('error', function (err) {
@@ -90,19 +90,12 @@ async function captureStreamImages() {
         });
     });
 }
-// ffmpeg -ss 0.5 -rtsp_transport tcp -i rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/401 -vframes 1 -f image2 imagefile.jpg
-// ffmpeg -rtsp_transport tcp -i rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/401 -ss 00:00:03 -t 00:00:08 -async 1 cut.mp4
-// ffmpeg -ss -rtsp_transport tcp -i rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/401 setpts=0.25*PTS -q:v 3 -update 1 -y test.png
-// ffmpeg -ss 00:08:00 -rtsp_transport tcp -i rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/401 -ss 00:01:00 -t 00:01:00 -c copy VideoClip.mp4
-//ffmpeg -rtsp_transport tcp -i rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/401 -vframes 5 -update 1
-//ffmpeg -rtsp_transport tcp -i rtsp://admin:ADMIN@12@136.233.89.172:1025/Streaming/Channels/401 -vframes 5 -f image2 -update 1 -
 
 async function readCameras() {
     let jsonData = await fs.readFileSync(__dirname + "/camera.json");
     return JSON.parse(jsonData);
 }
 
-// use rtsp = require('rtsp-ffmpeg') instead if you have install the package
-server.listen(3000, function () {
-    console.log('Listening on localhost:3000');
+server.listen(80, function () {
+    console.log('Listening on localhost:80');
 });
